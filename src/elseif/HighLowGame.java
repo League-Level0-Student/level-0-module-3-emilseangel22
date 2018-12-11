fea6928fa917991104c03c616a52a03141d978a4
337296 <337296@0338S-S-R0B6ZHR.sdcs.local>
2018-12-11 14:22:48 -0800

    HighLowGame ip

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,20 +5,21 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
+		int random = new Random().nextInt(101)+1;
 		// 2. Print out the random variable above
-		
+		int random = new Random().nextInt(5);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+ String answer = JOptionPane.showInputDialog(null, "What number am i thinking about from 1-100");
+			// 4. Convert the answer to an int (Integer.parseInt(string))
+			 int sn = Integer.parseInt(answer);
 			// 5. if the guess is correct
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
