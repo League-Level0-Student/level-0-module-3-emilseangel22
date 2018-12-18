2154591ed5505d277fa85454aaf4a2badd9827aa
337296 <337296@0338S-S-R0B6ZHR.sdcs.local>
2018-12-18 14:21:55 -0800

    HighLowGame ip

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -13,7 +13,7 @@ public class HighLowGame {
 		// 3. Change this line to give you a random number between 1 - 100. 
 		int random = new Random().nextInt(101)+1;
 		// 2. Print out the random variable above
-		int random = new Random().nextInt(5);
+System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
@@ -21,17 +21,20 @@ public class HighLowGame {
 			// 4. Convert the answer to an int (Integer.parseInt(string))
 			 int sn = Integer.parseInt(answer);
 			// 5. if the guess is correct
-				// 6. Win
+		if(sn==random) { JOptionPane.showMessageDialog(null, "you win");
+			 // 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
 				// 8. Tell them it's too high
+		}	else if(sn>=random) JOptionPane.showInputDialog(null, "Your number is too high");
 			// 9. if the guess is low
+		else if(sn=<random) {
 				// 10. Tell them it's too low
 
 		// 13. Tell them they lose
 		
 	}
-
+	}
 }
 
 
